2ff15c7314cd3511e0a2ecdc12a80bb526dd8cb6
Nestor162 <nestorcicardini@gmail.com>
2023-05-12 11:26:43 +0200

    Crea classe Book

---
+++ src/entities/Book.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Book extends Publication {
+	private String author;
+	private String genre;
+
+	public Book(String ISBN, String title, int publicationYear, int numPages,
+			String author, String genre) {
+		super(ISBN, title, publicationYear, numPages);
+		this.author = author;
+		this.genre = genre;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [author=" + author + ", genre=" + genre + ", ISBN=" + ISBN
+				+ ", title=" + title + ", publicationYear=" + publicationYear
+				+ ", numPages=" + numPages + "]";
+	}
+
+}

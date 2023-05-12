84d107bdbd0a67061eda09443366618a6d198b86
Nestor162 <nestorcicardini@gmail.com>
2023-05-12 11:26:59 +0200

    Crea superclasse Publication

---
+++ src/entities/Publication.java
@@ -0,0 +1,49 @@
+package entities;
+
+public abstract class Publication {
+	protected String ISBN;
+	protected String title;
+	protected int publicationYear;
+	protected int numPages;
+
+	protected Publication(String ISBN, String title, int publicationYear,
+			int numPages) {
+		this.ISBN = ISBN;
+		this.title = title;
+		this.publicationYear = publicationYear;
+		this.numPages = numPages;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public int getNumPages() {
+		return numPages;
+	}
+
+	public void setNumPages(int numPages) {
+		this.numPages = numPages;
+	}
+
+}
